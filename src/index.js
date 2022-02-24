b56570f788fb74171ee6c6996143bac520a96ee6
mofe <mofeomotosho8@gmail.com>
2022-02-24 14:51:32 -0300

    testing setup

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import  ReactDOM  from "react-dom";
+import Navbar from "./components/Navbar";
+import Main from "./components/Main";
+
+function App(){
+  return (
+    <>
+      <Navbar />
+      <Main />
+    </>
+  ) 
+}
+
+ReactDOM.render( <App />, document.getElementById("root"))
