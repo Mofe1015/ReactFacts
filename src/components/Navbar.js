d1ebe670a8f6ffbbb59a2e98241a2df4d60beac6
mofe <mofeomotosho8@gmail.com>
2022-02-24 16:24:26 -0300

    desiging NavBar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,8 +4,15 @@ import '../style.css'
 
 export default function Navbar(){
     return(
-        <nav>
-            <img src={require('../images/reactimage.png') }  className="img"/>
+        <nav className="Nav">
+            <div className="logo">
+                <img src={require('../images/reactimage.png') }  className="img"/>
+                <p className="txt" >ReactFacts</p>
+            </div>
+            <div className="madeby">
+                <p className="madebytxt">Developed by Mofe</p>
+            </div>
+
         </nav>
     )
     
