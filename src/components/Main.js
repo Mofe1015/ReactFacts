98678fe9467b32f930cf3a64d4126f37ef5090a1
mofe <mofeomotosho8@gmail.com>
2022-02-24 22:26:44 -0300

    designing Main(body)

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,8 +3,19 @@ import '../style.css'
 
 export default function Main(){
     return(
-        <div>
-            <h1>main content</h1>
+        <div className="Main">
+            <div className="mainhead">
+                <h1 className="maintxt">Facts about </h1>
+                <h1 className="maintxt1">React</h1>
+            </div>
+            
+            <ul className="main--facts">
+                <li>Was first released in 2013</li>
+                <li>Was originally created by Jordan Walke</li>
+                <li>Has well over 100K stars on GitHub</li>
+                <li>Is maintained by Facebook</li>
+                <li>Powers thousands of enterprise apps, including mobile apps</li>
+            </ul>
         </div>
     )
     
