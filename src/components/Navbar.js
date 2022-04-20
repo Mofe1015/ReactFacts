bfbf442ed708595ea11d71a70cb45b045c2232aa
mofe <mofeomotosho8@gmail.com>
2022-04-19 23:03:57 -0300

    adding dark and light mode

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,15 +2,25 @@ import React from "react";
 import '../style.css'
 
 
-export default function Navbar(){
+export default function Navbar(props){
     return(
-        <nav className="Nav">
+        <nav className={props.darkMode ? "dark": ""}>
             <div className="logo">
                 <img src={require('../images/reactimage.png') }  className="img"/>
                 <p className="txt" >ReactFacts</p>
             </div>
-            <div className="madeby">
-                <p className="madebytxt">Developed by Mofe</p>
+
+            <div 
+                className="toggler" 
+            >
+                <p className="toggler--light">Light</p>
+                <div 
+                    className="toggler--slider"
+                    onClick={props.toggleDarkMode}
+                >
+                    <div className="toggler--slider--circle"></div>
+                </div>
+                <p className="toggler--dark">Dark</p>
             </div>
 
         </nav>
